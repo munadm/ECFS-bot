4e27238cd604073bea361c4d797f2c90c57087f3
Munad Mahinoor <munad_707@yahoo.com>
2017-05-16 22:02:37 -0400

    Added Quick Reply interface and name follow through

--- app/models/handler.js
+++ app/models/handler.js
@@ -16,7 +16,7 @@ exports.postbackHandler = (event, senderId) => {
 		getUserInformation(senderId)
 		.then((userInfo) => { 
 			userInfo = JSON.parse(userInfo);
-	        sendTextMessage(senderId, `Your name is ${userInfo.first_name} ${userInfo.last_name}. Is that correct?`); 
+			InitializeConversation(senderId, userInfo);
 	      }) 
     	.catch((error) => { 
       		console.log(`Error getting userInfo messages: ${error}`); 
@@ -25,11 +25,57 @@ exports.postbackHandler = (event, senderId) => {
 	else if (payload === 'FAQ_DATA_USE') {
 		sendTextMessage(senderId, 'Great Question! Though we are storing your data in order to prepare your comment we will delete it right after you confirm to submit your comment.')
 	}
+	else if(payload === 'CORRECT_NAME') {
+		sendTextMessage(senderId, 'Great!');
+	}
+	else if(payload === 'INCORRECT_NAME') {
+		sendTextMessage(senderId, 'Darn :(');
+	}
+}
+
+function InitializeConversation(userInfo, senderId) {
+	const replyOptions = {
+		yes : 'CORRECT_NAME',
+		no : 'INCORRECT_NAME',
+	};
+	const message = `Your name is ${userInfo.first_name} ${userInfo.last_name}. Is that correct?`;
+	sendQuickReply(senderId, message, replyOptions);
 }
 
 function getUserInformation(senderId) {
 	const url = `https://graph.facebook.com/v2.6/${senderId}?fields=first_name,last_name&access_token=${token}`;
 	return rp(url);
+}
+
+function sendQuickReply(sender, title, options) {
+	let optionsData = [];
+	for (item in options) {
+		optionsData.push({
+			content_type : "text",
+			title : item,
+			payload : options[item],
+		});
+	}
+	console.log(JSON.stringify(optionsData));
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json: {
+	    	recipient: {id:sender},
+	    	message: {
+	    		text: title,
+	    		quick_replies: optionsData
+
+	    	}
+	    }
+	}, function(error, response, body) {
+		if (error) {
+		    console.log(`Error sending messages: ${JOSN.stringify(error)}`);
+		} else if (response.body.error) {
+		    console.log(`Error: ${response.body.error}`);
+	    }
+	});
 }
 
 function sendTextMessage(sender, text) {
@@ -48,5 +94,5 @@ function sendTextMessage(sender, text) {
 		} else if (response.body.error) {
 		    console.log(`Error: ${response.body.error}`);
 	    }
-    })
+    });
 }
