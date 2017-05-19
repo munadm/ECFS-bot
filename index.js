5ed6bc829c001f6dc47c0fa69cd3439c8349c7ab
Munad Mahinoor <munad_707@yahoo.com>
2017-05-18 22:54:12 -0400

    Added mongo package

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict'
 
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
 const baseRoutes = require('./app/routes/baseRouter')
 const fbRoutes = require('./app/routes/fbRouter');
 
 const app = express();
+
+const uristring = process.env.MONGODB_URI ||
+			    process.env.MONGOHQ_URL ||
+			    'mongodb://localhost/ECFS_BOT';
 
 app.set('port', (process.env.PORT || 5550))
 
@@ -19,6 +24,15 @@ app.use(bodyParser.json());
 //Routes
 app.use('/', baseRoutes);
 app.use('/webhook', fbRoutes);
+
+//Connect to mongodb
+mongoose.connect(uristring, (err, res) => {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+  });
 
 // Spin up the server
 app.listen(app.get('port'), function() {
